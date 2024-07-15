c68c923c48d19d5005575b324855e3d15637b60e
toseeq44 <toseequrrehman903@gmail.com>
2024-07-15 19:49:29 +0500

    basic functionality done

--- scripts/main.py
+++ scripts/main.py
@@ -72,4 +72,9 @@ def main():
     listener_thread.join()
 
 if __name__ == "__main__":
-    main()
+    try:
+        main()
+    except Exception as e:
+        print(f"An error occurred: {e}")
+    finally:
+        input("Press Enter to close...")
